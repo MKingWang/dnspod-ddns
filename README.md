965eaf7fa6fd467735660dc9e1f984c074dd9749
mking <mk@MKs-MacBookPro-E570.local>
2019-01-19 21:40:22 +0800

    v1

---
+++ README.md
@@ -0,0 +1,20 @@
+# ddns by dnspod
+
+## 0x00 安装
+build后的二进制文件和配置文件，放在任意位置（看各位客官的喜好）
+
+## 0x01 配置说明
+```ini
+[Dnspod] ;Dnspod配置信息头，不能改动
+token = id,token ;dnspod上申请的id和token组合
+format = json  ;返回格式，默认json
+domainid = 域名的id; 可以通过记录列表获取 参考手册：https://www.dnspod.cn/docs/records.html#record-list
+recordid = 记录id;可以通过记录列表获取 参考手册：https://www.dnspod.cn/docs/records.html#record-list 
+subdomain = www;记录值 默认www, 留空为@
+```
+
+## 0x02 crontab配置
+```bash
+#例如：将二进制文件和配置文件放在,/opt/dnspod
+*/10 * * * * cd /opt/dnspod;./dnspod-dns
+```
---
+++ main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/go-ini/ini"
+)
+
+type dnspodMsg struct {
+	Status status `json:"status"`
+}
+
+type status struct {
+	Code string `json:"code"`
+	Msg  string `json:"message"`
+}
+
+type config struct {
+	Dndpod dnspod `ini:"Dnspod"`
+	Email  email  `ini:"Email"`
+}
+type dnspod struct {
+	Token     string `ini:"token"`
+	Format    string `ini:"format"`
+	Domainid  string `ini:"domainid"`
+	Recordid  string `ini:"recordid"`
+	Subdomain string `ini:"subdomain"`
+}
+type email struct {
+}
+
+const (
+	ipfile = "/tmp/ip"
+	ipurl  = "http://ifconfig.me"
+)
+
+func init() {
+	_, err := os.Stat(ipfile)
+	if os.IsNotExist(err) {
+		file, _ := os.Create(ipfile)
+		defer file.Close()
+	}
+}
+
+func main() {
+	oldip, _ := ioutil.ReadFile(ipfile)
+	ip := getip()
+	if ip == string(oldip) {
+		os.Exit(0)
+	}
+	config := getconfig()
+	ddns(config.Dndpod)
+	os.Truncate(ipfile, 0)
+	ioutil.WriteFile(ipfile, []byte(ip), 0644)
+	// fmt.Println(ip)
+}
+
+// 获取当前公网ip
+func getip() string {
+	c := &http.Client{}
+	request, err := http.NewRequest("GET", ipurl, nil)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	response, _ := c.Do(request)
+	ip, _ := ioutil.ReadAll(response.Body)
+	return string(ip)
+}
+
+// 修改dnspod域名
+func ddns(dnspodinfo dnspod) *status {
+	v := url.Values{}
+	c := &http.Client{}
+	v.Set("login_token", dnspodinfo.Token)
+	v.Add("format", dnspodinfo.Format)
+	v.Add("domain_id", dnspodinfo.Domainid)
+	v.Add("record_id", dnspodinfo.Recordid)
+	v.Add("record_line", "默认")
+	v.Add("sub_domain", dnspodinfo.Subdomain)
+	body := ioutil.NopCloser(strings.NewReader(v.Encode()))
+	request, err := http.NewRequest("POST", "https://dnsapi.cn/Record.Ddns", body)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	request.Header.Add("Content-type", "application/x-www-form-urlencoded")
+	request.Header.Add("Accept", "text/json")
+	response, _ := c.Do(request)
+	msg := dnspodMsg{}
+	result, _ := ioutil.ReadAll(response.Body)
+	// result, _ = ioutil.ReadAll(request.Body)
+	json.Unmarshal(result, &msg)
+	fmt.Println(string(result))
+	return &msg.Status
+}
+
+// 从配置文件读取dnspod信息和邮箱信息
+func getconfig() *config {
+	dnspodconfig := new(config)
+	ini.MapTo(dnspodconfig, "config.ini")
+	return dnspodconfig
+}
+
+// 有时间完善如下内容
+// 修改失败 发送告警邮件到指定邮箱
+func sendmail() {
+
+}
